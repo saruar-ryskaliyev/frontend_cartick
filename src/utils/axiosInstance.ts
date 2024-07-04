ebf816e4a3d009a28eabc6bd6cfa2e02a446ae12
Saruar <saruar.ryskaliyev@nu.edu.kz>
2024-07-05 03:33:51 +0500

    done

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000', // your backend URL
+  baseURL: 'https://backend-cartick.onrender.com', // your backend URL
 });
 
 axiosInstance.interceptors.request.use(
